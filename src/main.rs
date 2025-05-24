72d936352f89aa06951afcf5cb7479f8549eec46
Rob Pellegrin <>
2025-05-23 21:55:10 -0400

    Removed leading block comment

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,6 @@
 /// Repository:
 /// =====================================================================
 ///
-/// # Overview
-/// This project enhances the traditional `rm` command by adding a trash bin
-/// feature. Files deleted with the `rm` command are instead moved to a trash
-/// bin directory, making them recoverable if deleted by accident. The tool
-/// also integrates with GUI-based trash bins (e.g., Dolphin) so that users
-/// can restore files using their desktop environment's trash interface.
-///
-/// ## Key Features:
-/// - Trash bin for `rm`-deleted files, allowing recovery. This is currently located
-/// at ~/trash
-/// - Command-line interface mimics the behavior of the traditional `rm` command.
-///
-/// ## TODO
-/// - Handle command line arguments to view, empty and list trash contents.
-/// - Store metadata along with deleted files to make restoring easier.
-/// - Restore files from trash using CLI
-/// - View trash contents via CLI.
-/// - Config file to allow user to specify custom path to trash, size limitations, etc.
-/// - Integration with GUI trash bins (e.g., Dolphin, Nautilus) for restoration.
 mod args;
 mod mv;
 mod view;
