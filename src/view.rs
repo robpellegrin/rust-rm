de69f4a1e90e11097f98e50b7b6adb8b8b3db29f
rob <>
2025-05-29 15:58:33 -0400

    Removed unused table function

--- src/view.rs
+++ src/view.rs
@@ -15,49 +15,6 @@ use std::io::{BufRead, BufReader};
 use tabled::settings::style::Style;
 use tabled::{Table, Tabled};
 
-#[allow(unused)]
-// NOTE!!
-// This function will likely be removed in favor of the function below that
-// prints out the trash contents in a table.
-//
-pub fn list_trash_contents() {
-    // Get the path to the trash directory
-    let home_dir = dirs_next::home_dir().expect("Failed to get home directory");
-    let trash_dir = home_dir.join(".local/share/Trash/files");
-
-    // Check if the trash directory exists
-    if !trash_dir.exists() {
-        println!("Trash directory does not exist at {:?}", trash_dir);
-        return;
-    }
-
-    // List all files and directories in the trash directory
-    match fs::read_dir(trash_dir) {
-        Ok(entries) => {
-            let mut has_files = false;
-            for entry in entries {
-                match entry {
-                    Ok(entry) => {
-                        let entry_name = entry.file_name();
-                        println!("{}", entry_name.to_string_lossy());
-                        has_files = true;
-                    }
-                    Err(e) => {
-                        println!("Failed to read entry: {}", e);
-                    }
-                }
-            }
-
-            if !has_files {
-                println!("The trash is empty.");
-            }
-        }
-        Err(e) => {
-            println!("Failed to read the trash directory: {}", e);
-        }
-    }
-}
-
 #[derive(Tabled)]
 struct TrashEntry {
     file: String,
@@ -110,7 +67,7 @@ pub fn list_trash_contents_table() {
     }
 }
 
-/// Reads the original file path from the corresponding .trashinfo file
+/// Extracts info from .trashinfo file. i.e. path or datetime.
 fn get_info_from_trashinfo(file_name: &str, search_term: &str) -> Option<String> {
     let home_dir = dirs_next::home_dir()?;
     let info_path = home_dir
